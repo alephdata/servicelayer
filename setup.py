c9141ec7a35301b675d6753af6db720b7414979a
Friedrich Lindenberg <friedrich@pudo.org>
2019-02-02 12:29:43 +0100

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='servicelayer',
-    version='0.0.0',
+    version='0.1.0',
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
