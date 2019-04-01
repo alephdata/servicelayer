3d3e8ac4d699913cdd471c2fbaa7d1f64b595790
jen <jen@occrp.org>
2019-04-01 18:40:15 +0200

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='servicelayer',
-    version='1.0.2',
+    version='1.0.3',
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
