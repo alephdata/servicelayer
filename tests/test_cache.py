627080d2c0a5b6cd45c31de6caa0a8ccb386bb42
Tarashish Mishra <sunu0000@gmail.com>
2019-04-08 12:44:25 +0530

    Use  instead of loops

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -33,23 +33,19 @@ class CacheTest(TestCase):
             'total': 2,
             'finished': 0,
         }
-        # weird work-around because `yield from` doesn't work inside pytest
-        # pytest just skips the test without any warning
-        for task in poll_task():
-            assert task == (dataset, entity1, config)
-            mark_task_finished(dataset)
-            assert get_status(dataset) == {
-                'total': 2,
-                'finished': 1
-            }
-            break
-        for task in poll_task():
-            # Test that marking all tasks as finished, resets the dataset
-            mark_task_finished(dataset)
-            conn = get_redis()
-            assert get_status(dataset) == {
-                'total': 0,
-                'finished': 0,
-            }
-            assert not conn.exists(make_key('ingest', 'pending', dataset))
-            break
+        task = next(poll_task())
+        assert task == (dataset, entity1, config)
+        mark_task_finished(dataset)
+        assert get_status(dataset) == {
+            'total': 2,
+            'finished': 1
+        }
+        task = next(poll_task())
+        # Test that marking all tasks as finished, resets the dataset
+        mark_task_finished(dataset)
+        conn = get_redis()
+        assert get_status(dataset) == {
+            'total': 0,
+            'finished': 0,
+        }
+        assert not conn.exists(make_key('ingest', 'pending', dataset))
