b429c449818300370c90169f5a4f1af11e0ee1da
Friedrich Lindenberg <friedrich@pudo.org>
2019-06-27 13:27:12 +0200

    More defensive

--- servicelayer/archive/util.py
+++ servicelayer/archive/util.py
@@ -4,9 +4,16 @@ from pathlib import Path
 BUF_SIZE = 1024 * 1024 * 16
 
 
+def ensure_path(file_path):
+    if file_path is None or isinstance(file_path, Path):
+        return file_path
+    return Path(file_path).resolve()
+
+
 def checksum(file_name):
     """Generate a hash for a given file name."""
-    if file_name is not None:
+    file_name = ensure_path(file_name)
+    if file_name is not None and file_name.is_file():
         digest = sha1()
         with open(file_name, 'rb') as fh:
             while True:
@@ -15,9 +22,3 @@ def checksum(file_name):
                     break
                 digest.update(block)
         return str(digest.hexdigest())
-
-
-def ensure_path(file_path):
-    if file_path is None or isinstance(file_path, Path):
-        return file_path
-    return Path(file_path).resolve()
