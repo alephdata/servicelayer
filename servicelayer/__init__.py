1455bc305302ee184695af006477f9812ff39910
Friedrich Lindenberg <friedrich@pudo.org>
2020-01-14 17:41:41 +0100

    Bump version: 1.9.5 → 1.9.6

--- servicelayer/__init__.py
+++ servicelayer/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = '1.9.5'
+__version__ = '1.9.6'
 
 
 logging.getLogger('boto3').setLevel(logging.WARNING)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='servicelayer',
-    version='1.9.5',
+    version='1.9.6',
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
