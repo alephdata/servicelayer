fadd79366634834ef35e3fa80360cacc80930ac4
Friedrich Lindenberg <friedrich@pudo.org>
2021-01-26 18:12:34 +0100

    lock ranges

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
     include_package_data=True,
     zip_safe=True,
     install_requires=[
-        "banal >= 1.0.1",
-        "normality >= 2.1.1",
+        "banal >= 1.0.1, <2.0.0",
+        "normality >= 2.1.1, <3.0.0",
         "fakeredis == 1.4.5",
         "sqlalchemy >= 1.3",
         "structlog >= 20.2.0, <21.0.0",
