5bdc67d87a2b8bc6ada9c5aa4b37b1b7b7790f70
Tarashish Mishra <sunu0000@gmail.com>
2022-07-13 18:02:55 +0530

    Bump version: 1.19.1 → 1.20.0

--- servicelayer/__init__.py
+++ servicelayer/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "1.19.1"
+__version__ = "1.20.0"
 
 
 logging.getLogger("boto3").setLevel(logging.WARNING)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="servicelayer",
-    version="1.19.1",
+    version="1.20.0",
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
