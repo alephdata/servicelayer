3f8ca464696c36aa0ce24d50b9471ed7488c8e77
Tarashish Mishra <sunu0000@gmail.com>
2022-08-08 15:13:10 +0530

    Bump version: 1.20.0 → 1.20.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="servicelayer",
-    version="1.20.0",
+    version="1.20.1",
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
