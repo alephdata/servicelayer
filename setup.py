ae76c103a70af6d3d06695f9c8636b908841d40a
catileptic <s.alexandra.stefanescu@gmail.com>
2023-08-24 17:06:28 +0300

    Fix SQLAlchemy calls  (#105)
    * Fix SQLAlchemy calls so that all cursor operations happen in the context of an open DB connection
    
    * Cap redis at previous version

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setup(
     install_requires=[
         "banal >= 1.0.6, < 2.0.0",
         "normality >= 2.4.0, < 3.0.0",
-        "fakeredis >=2.11.2, < 3.0.0",
+        "fakeredis >= 2.11.2, < 3.0.0",
+        "redis <= 4.6.0",
         "sqlalchemy >= 1.4.49, < 3.0.0",
         "structlog >= 20.2.0, < 24.0.0",
         "colorama >= 0.4.4, < 1.0.0",
