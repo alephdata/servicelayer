87ffe7eff8cf1ad84c270bc9abe750f6330042ff
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-06-06 10:29:44 +0200

    Bump version: 1.23.0-rc21 → 1.23.0-rc22

--- servicelayer/__init__.py
+++ servicelayer/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "1.23.0-rc21"
+__version__ = "1.23.0-rc22"
 
 logging.getLogger("boto3").setLevel(logging.WARNING)
 logging.getLogger("botocore").setLevel(logging.WARNING)
