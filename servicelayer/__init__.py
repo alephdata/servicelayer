8fa15bc9acf8d7b3f772aebe5533f6e94cb1e956
Alex Stefanescu <alex.stefanescu@protonmail.com>
2024-06-19 15:42:53 +0300

    Bump version: 1.23.0-rc24 → 1.23.0-rc25

--- servicelayer/__init__.py
+++ servicelayer/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "1.23.0-rc24"
+__version__ = "1.23.0-rc25"
 
 logging.getLogger("boto3").setLevel(logging.WARNING)
 logging.getLogger("botocore").setLevel(logging.WARNING)
