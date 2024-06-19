f34fe01f36cf1779976cbc306db34959b61a5210
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-06-19 10:31:18 +0200

    Bump version: 1.23.0-rc23 → 1.23.0-rc24

--- servicelayer/__init__.py
+++ servicelayer/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "1.23.0-rc23"
+__version__ = "1.23.0-rc24"
 
 logging.getLogger("boto3").setLevel(logging.WARNING)
 logging.getLogger("botocore").setLevel(logging.WARNING)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="servicelayer",
-    version="1.23.0-rc23",
+    version="1.23.0-rc24",
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
