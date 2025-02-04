6b51be2d11fc702b9315db668d12748f80310359
Christian Stefanescu <christian.stefanescu@occrp.org>
2025-02-04 10:30:25 +0200

    Bump version: 1.23.3 → 1.24.0-rc1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="servicelayer",
-    version="1.23.3",
+    version="1.24.0-rc1",
     description="Basic remote service functions for alephdata components",
     classifiers=[
         "Development Status :: 3 - Alpha",
